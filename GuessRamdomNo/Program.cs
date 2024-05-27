6a4b4aecd08dc2e9ce61a97410a2e23c9dee8f1b
dell <anjalisangekar1998@gmail.com>
2024-05-27 18:23:13 +0530

    Add project files.

---
+++ GuessRamdomNo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessRamdomNo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var randomNumber = (new Random()).Next(10);
+            Console.WriteLine("Random number: {0}", randomNumber);
+
+            for (var i = 1; i <= 4; i++)
+            {
+                Console.Write("Guess the number between 1 and 10: ");
+                var input = Convert.ToInt32(Console.ReadLine());
+                if (input == randomNumber)
+                {
+                    Console.WriteLine("Yow won");
+                    break;
+                }
+                else if (i == 4)
+                {
+                    Console.WriteLine("Yow lost");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+
+}
